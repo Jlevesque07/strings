ce38091a8e339b8335c85bb921ac739ed318e7a6
Jlevesque07 <joshual251@deltalearns.ca>
2025-02-06 21:17:27 +0000

    added Value errors and finished program

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 
 # This program will calculate expenses and expendetures
 
-
-
-# Ask for monthly allowance and expenses in different categories
-income = float(input("What is your monthly income? $"))
-food = float(input("What is your monthly food spending? $"))
-clothes = float(input("What is your monthly clothing spending? $"))
-entertainment = float(input("What is your monthly entertainment spending? $"))
-
+while True:
+    try:
+    # Ask for monthly allowance and expenses in different categories
+        income = float(input("What is your monthly income? $"))
+        food = float(input("What is your monthly food spending? $"))
+        clothes = float(input("What is your monthly clothing spending? $"))
+        entertainment = float(input("What is your monthly entertainment spending? $"))
+        break
+    except ValueError:
+        print("Invalid number. ")
 
 # Calculate total expenses and remaining balance
 expenses = income + food + clothes + entertainment
@@ -22,3 +24,7 @@ print(f"Remaining is : ${balance:.2f}")
 
 
 # Conditional message based on balance (depending on whether you overspent or underspent or spent just the right amount, write a specific message - try to be kind!)
+if balance <= 0:
+    print("You spent too much money ")
+elif balance > 0:
+    print("Good job, you didn't overspend. ")
